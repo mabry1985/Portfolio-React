6242e14f1c88e555f2aa043aa3fd5965465c2a43
Josh Mabry <mabry1985@gmail.com>
2020-02-03 15:19:24 -0800

    add conditional render for landscape mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { useContext } from 'react';
 
-import NavBar from './PageContainer';
-import LandingPage from './LandingPage';
+import ResponsiveNav from './components/navigation/ResponsiveNav';
+import LandingPage from './components/LandingPage';
+import MainContent from './components/MainContent';
+import LandscapeMode from './components/responsive/LandscapeMode';
+
 import { VisibilityContext } from './contexts/VisibilityContext';
-import MainContent from './MainContent';
+import { WindowContext } from './contexts/WindowContext';
 
 const App = () => {
   const { navBarVisible } = useContext(VisibilityContext);
-
+  const { landscapeMode } = useContext(WindowContext);
   let content;
-  
-if (navBarVisible === true) {
-    content = (
-      <React.Fragment>
-        <NavBar/>
-        <MainContent/>
-      </React.Fragment>
-    )
-  } else {
-    content = <LandingPage/>
-  }
+
+  if (landscapeMode) {
+    content = <LandscapeMode/>
+  } else if (!landscapeMode && navBarVisible === true) {
+      content = (
+        <React.Fragment>
+          <ResponsiveNav/>
+          <MainContent/>
+        </React.Fragment>
+      )
+    } else if (!landscapeMode) {
+      content = <LandingPage/>
+    }
 
   return (
     <div className="App">
