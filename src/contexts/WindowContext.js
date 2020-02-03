90cc6b3dfffbaf541f934accefa420cd3d19336d
Josh Mabry <mabry1985@gmail.com>
2020-02-03 15:20:08 -0800

    add state and function to determine if mobile is in landscape mode

--- src/contexts/WindowContext.js
+++ src/contexts/WindowContext.js
@@ -7,16 +7,26 @@ const WindowContextProvider = (props) => {
   const size = useWindowSize();
 
   const [windowSize, changeWindowSize] = useState(size);
+  const [landscapeMode, toggleLandscape] = useState(false);
 
   useEffect(() => {
     changeWindowSize(size);
+    toggleLandscape(handleLandscapeToggle(size));
   }, [size])
 
+  const handleLandscapeToggle = (size) =>{
+    if (size.width > size.height && size.height < 766) {
+      return true;
+    } else if (size.width < size.height && size.width < 766) {
+      return false;
+    }
+  }
+
   return (
-    <WindowContext.Provider value={{ windowSize }}>
+    <WindowContext.Provider value={{ windowSize, landscapeMode }}>
       {props.children}
     </WindowContext.Provider>
-  )
+  );
 }
 
 export default WindowContextProvider
